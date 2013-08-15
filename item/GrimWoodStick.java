b5608da3793315fae9a462271c6063569ba7d1e7
Lapiman <xyz2010li@gmail.com>
2013-08-15 17:02:17 -0400

    Texture fix attempt
    Again.

--- item/GrimWoodStick.java
+++ item/GrimWoodStick.java
@@ -17,6 +17,6 @@ public class GrimWoodStick extends Item {
         @Override
         @SideOnly(Side.CLIENT)
         public void registerIcons(IconRegister iconRegister) {
-                 this.itemIcon = iconRegister.registerIcon("grimcraft:grimwoodstick");
+                 this.itemIcon = iconRegister.registerIcon("GrimCraft:grimwoodstick");
         }
 }
