32317b64a26eecfd2804af6c0029350020599d07
Lapiman <xyz2010li@gmail.com>
2013-08-22 02:31:35 -0400

    Create ItemChilipepper.java

---
+++ ItemChiliPepper.java
@@ -0,0 +1,22 @@
+package thinkplank.grimcraft.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.texture.IconRegister;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ItemChiliPepper extends Item {
+        public ItemChiliPepper(int id) {
+                super(id);
+        		setMaxStackSize(64);
+        		setCreativeTab(CreativeTabs.tabMaterials);
+        		setUnlocalizedName("chilipepper");
+        }
+        
+        @Override
+        @SideOnly(Side.CLIENT)
+        public void registerIcons(IconRegister iconRegister) {
+                 this.itemIcon = iconRegister.registerIcon("GrimCraft:chilipepper");
+        }
+}
