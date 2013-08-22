e7d1fe9f8c87c180105e8e323ff21cd9a4d17eea
Lapiman <xyz2010li@gmail.com>
2013-08-22 03:34:25 -0400

    Create BlockSulfurOre.java
    Sure be nice if I could get it on my first try :-)

---
+++ block/BlockSulfurOre.java
@@ -0,0 +1,27 @@
+package thinkplank.grimcraft.block;
+
+import java.util.Random;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.texture.IconRegister;
+
+public class BlockSulfurOre extends Block {
+        public GenericBlock (int id, Material material) {
+                super(id, material);
+                setHardness(3F);
+                setStepSound(Block.soundStoneFootstep);
+                setUnlocalizedName("sulfurore");
+                setCreativeTab(CreativeTabs.tabBlock);
+        }
+        
+        public int idDropped(int par1, Random random, int zero) {
+                return Grimcraft.sulfur.itemID;
+        }
+        
+        @Override
+        @SideOnly(Side.CLIENT)
+        public void registerIcons(IconRegister iconRegister) {
+                 this.itemIcon = iconRegister.registerIcon("GrimCraft:sulfurore");
+        }
+}
