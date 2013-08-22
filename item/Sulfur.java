f486a45f8574c213cfe13328cfd105239d37ed89
Lapiman <xyz2010li@gmail.com>
2013-08-22 02:21:09 -0400

    Create Sulfur.java

---
+++ item/Sulfur.java
@@ -0,0 +1,22 @@
+package thinkplank.grimcraft.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.texture.IconRegister;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class Sulfur extends Item {
+        public Sulfur(int id) {
+                super(id);
+        		setMaxStackSize(64);
+        		setCreativeTab(CreativeTabs.tabMaterials);
+        		setUnlocalizedName("sulfur");
+        }
+        
+        @Override
+        @SideOnly(Side.CLIENT)
+        public void registerIcons(IconRegister iconRegister) {
+                 this.itemIcon = iconRegister.registerIcon("GrimCraft:sulfur");
+        }
+}
