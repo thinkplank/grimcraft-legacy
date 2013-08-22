f44eac2d4b06947e2bab7948f9d5f0ff31e63117
Lapiman <xyz2010li@gmail.com>
2013-08-22 02:30:04 -0400

    Create ItemStrawberry.java

---
+++ item/ItemStrawberry.java
@@ -0,0 +1,22 @@
+package thinkplank.grimcraft.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.texture.IconRegister;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ItemStrawberry extends Item {
+        public ItemStrawberry(int id) {
+                super(id);
+        		setMaxStackSize(64);
+        		setCreativeTab(CreativeTabs.tabMaterials);
+        		setUnlocalizedName("strawberry");
+        }
+        
+        @Override
+        @SideOnly(Side.CLIENT)
+        public void registerIcons(IconRegister iconRegister) {
+                 this.itemIcon = iconRegister.registerIcon("GrimCraft:strawberry");
+        }
+}
