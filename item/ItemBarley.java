6fd0cca03383d8202aecb797b8635bee240e2852
Lapiman <xyz2010li@gmail.com>
2013-08-22 02:22:48 -0400

    Create ItemBarley.java

---
+++ item/ItemBarley.java
@@ -0,0 +1,22 @@
+package thinkplank.grimcraft.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.texture.IconRegister;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ItemBarley extends Item {
+        public ItemBarley(int id) {
+                super(id);
+        		setMaxStackSize(64);
+        		setCreativeTab(CreativeTabs.tabMaterials);
+        		setUnlocalizedName("barley");
+        }
+        
+        @Override
+        @SideOnly(Side.CLIENT)
+        public void registerIcons(IconRegister iconRegister) {
+                 this.itemIcon = iconRegister.registerIcon("GrimCraft:barley");
+        }
+}
